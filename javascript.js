2fb401fcf6a7795f3c2b7e62b05d431ac8025ed2
anshi43 <83951554+anshi43@users.noreply.github.com>
2023-03-18 13:26:26 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,44 @@
+let string = "";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+  button.addEventListener('click', (e)=>{
+    if(e.target.innerHTML == '='){
+      string = eval(string);
+      document.querySelector('input').value = string;
+      
+    }
+    else if(e.target.innerHTML == 'C'){
+      string = ""
+      document.querySelector('input').value = string;
+    }
+    else{ 
+    console.log(e.target)
+    string = string + e.target.innerHTML;
+    document.querySelector('input').value = string;
+      }
+
+    if(e.target.innerHTML == '+'  ){
+
+      if(e.target.innerHTML == "*"){
+
+        if(e.target.innerHTML == '='){
+          string = "error";
+          document.querySelector('input').value = string;
+          
+        }
+
+
+
+      }
+
+
+     
+    }
+    
+
+
+    
+  })
+})
+  
+
